8bb085fe834bd91cbcf8f2911911bee96bbbca30
arc-knl <arc-knl@localhost.localdomain>
2016-12-22 16:29:00 +0900

    modify parse log

--- scripts/parser/gcStatFromLog.py
+++ scripts/parser/gcStatFromLog.py
@@ -4,20 +4,10 @@ import sys, os, re
 import argparse
 
 class gcStat:
+    # total_mut_time = 0
     cur_alloc_size = 0
-    cur_ygc_time = 0
-    cur_fgc_time = 0
-    total_ygc_time = 0
-    total_fgc_time = 0
-    total_num_ygc = 0
-    total_num_fgc = 0
-    total_num_gc = 0
-    total_gc_time = 0
-    total_mut_time = 0
     total_exec_time = 0.0
     total_sys_time = 0.0
-    total_alloc_size = 0
-    allocation_rate = 0
     instructions= 0.0
     l2hits = 0.0
     l2misses= 0.0
@@ -26,6 +16,16 @@ class gcStat:
     cycles= 0.0
     mpki = 0.0
     cpi = 0.0
+    bandwidth = 0.0
+
+    cur_ygc_time = []
+    cur_fgc_time = []
+    total_ygc_time = []
+    total_fgc_time = []
+    total_num_ygc = []
+    total_num_fgc = []
+    total_alloc_size = []
+    allocation_rate = []
     
     total_acnt = 0
     line_cnt = 0
@@ -42,30 +42,30 @@ class gcStat:
     	line = re.sub('s', ' ', line)
     	return re.split('\s*', line)
 
-    def parse_gclog(self, inf, outf):
+    def parse_gclog(self, inf, outf, numlog):
         outf.write("# TimeStamp, YGC/FGC, Footprint, Reclaimed, Elapsed Time \n")
+        self.total_num_ygc.append(0)
+        self.total_num_fgc.append(0)
+        self.total_alloc_size.append(0)
+        self.total_ygc_time.append(0.0)
+        self.total_fgc_time.append(0.0)
         for line in inf:
             t = self.parse_line(line)
             if len(t) > 2:
-                if t[1] == "[GC":
-                    self.total_num_ygc += 1
-                    self.cur_alloc_size = int(t[7], 10) - int(t[8], 10)
-                    self.cur_ygc_time = float(t[11])
-                    self.total_alloc_size += self.cur_alloc_size
-                    self.total_ygc_time += self.cur_ygc_time
-                    if args.gcdump:
-                      # fprint tuple (timestamp, y/f, footprint, reclaimed, time) to logfile
-                      t[0] = re.sub(':', '', t[0])
-                      outf.write(t[0] + ",Y," + t[7] + "," + str(self.cur_alloc_size) + "," + str(self.cur_ygc_time) + "\n")
-                elif t[1] == "[Full":
-                    self.total_num_fgc += 1
-                    self.cur_alloc_size = int(t[13], 10) - int(t[14], 10)
-                    self.total_fgc_time += float(t[23])
-                    self.total_alloc_size += self.cur_alloc_size
-                    if args.gcdump:
-                      # fprint tuple (timestamp, y/f, footprint, reclaimed, time) to logfile
-                      t[0] = re.sub(':', ' ', t[0])
-                      outf.write(t[0] + ",F," + t[13] + "," + str(self.cur_alloc_size) + "," + str(float(t[23])) + "\n")
+                if t[1] == "[GC" and len(t) > 10:
+                    self.total_num_ygc[numlog] += 1
+                    self.total_ygc_time[numlog] += float(t[11])
+                    self.total_alloc_size[numlog] += int(t[7], 10) - int(t[8], 10)
+                    # fprint tuple (timestamp, y/f, footprint, reclaimed, time) to logfile
+                    t[0] = re.sub(':', '', t[0])
+                    outf.write(t[0] + ",Y," + t[7] + "," + str(int(t[7], 10) - int(t[8], 10)) + "," + t[11] + "\n")
+                elif t[1] == "[Full" and len(t) > 22:
+                    self.total_num_fgc[numlog] += 1
+                    self.total_fgc_time[numlog] += float(t[23])
+                    self.total_alloc_size[numlog] += int(t[13], 10) - int(t[14], 10)
+                    # fprint tuple (timestamp, y/f, footprint, reclaimed, time) to logfile
+                    t[0] = re.sub(':', ' ', t[0])
+                    outf.write(t[0] + ",F," + t[13] + "," + str(int(t[13], 10) - int(t[14], 10)) + "," + t[23] + "\n")
     
     def parse_jvmlog(self, inf):
         for line in inf:
@@ -75,6 +75,11 @@ class gcStat:
                     self.total_exec_time = float(t[1])*60 + float(t[2])
                 elif t[0] == "sys":
                     self.total_sys_time = float(t[1])*60 + float(t[2])
+
+                # for stream benchmark
+                if (t[0] == "Total") and (t[3] == "bandwidth:"):
+                  t = re.sub('MB/', '', t[5])
+                  self.bandwidth = int(t, 10)
                 
                 #t = re.sub(' ', '',  line)
                 t = re.sub('\t', ' ', line)
@@ -128,14 +133,31 @@ class gcStat:
                     inf_n.seek(0)
                     break
 
-    def print_on(self, outf):
-        self.total_gc_time =  self.total_ygc_time  + self.total_fgc_time
-        self.total_num_gc =   self.total_num_ygc   + self.total_num_fgc
-        self.total_gc_time =  self.total_ygc_time  + self.total_fgc_time
-        self.total_mut_time = self.total_exec_time - (self.total_ygc_time + self.total_fgc_time)
-        self.allocation_rate = float(self.total_alloc_size) / \
-        (float(self.total_exec_time) / float(self.total_num_gc))
+    def parse_vtune_stat(self, inf, outf):
+      while True:
+        line = inf.readline()  
+        if line == "General Exploration Metrics\n":
+          while True:
+            line = inf.readline()
+            if line == "Collection and Platform Info\n": break 
+            print line 
+            outf.write(line)
+            
 
+        if line == "Summary\n":
+          while True:
+            line = inf.readline()
+            if line == "Event summary\n": break 
+            print line
+            outf.write(line)
+            line = re.sub(' ', ' ', line)
+            t = re.split('\s*', line)
+            if t[0] == "Elapsed":
+              self.total_exec_time = float(t[2])
+
+        if not line: break #EOF
+      
+    def print_on(self, outf):
         self.mpki = 0 if self.instructions == 0 else \
                     (float(self.l2misses) / float(self.instructions)) * 1000
         self.cpi  = 0 if self.instructions == 0 else \
@@ -146,52 +168,62 @@ class gcStat:
                     float(self.l2misses_pf) / (float(self.l2hits_pf) + float(self.l2misses_pf))
 
         print "Total exec time:      ", self.total_exec_time
-        print "Total young GC time : ", self.total_ygc_time
-        print "Total full GC time:   ", self.total_fgc_time
-        print "Total GC time:        ", self.total_gc_time
-        print "--# of Young GC:      ", self.total_num_ygc
-        print "--# of Full GC:       ", self.total_num_fgc
-        print "--# of GC:            ", self.total_num_gc
-        print "--Total GC time:      ", self.total_gc_time
-        print "Tatal Mutator time:   ", self.total_mut_time
-        print "Total alloc size:     ", self.total_alloc_size
-        print "Allocation rate:      ", self.allocation_rate
         print "MPKI (L2):            ", self.mpki
         print "CPI:                  ", self.cpi
         print "L2 miss rate (ret):   ", self.l2missrate
         print "L2 miss rate (pf):    ", self.l2missrate_pf
-        
+        if (self.bandwidth != 0):
+          print "Bandwidth (MB/sec):   ", self.bandwidth
+
         outf.write("Total exec time:     \t " + str(self.total_exec_time) + "\n")
-        outf.write("Total young GC time: \t " + str(self.total_ygc_time) + "\n")
-        outf.write("Total full GC time:  \t " + str(self.total_fgc_time) + "\n")
-        outf.write("Total GC time:       \t " + str(self.total_gc_time) + "\n")
-        outf.write("--# of Young GC:     \t " + str(self.total_num_ygc) + "\n")
-        outf.write("--# of Full GC:      \t "+  str(self.total_num_fgc) + "\n")
-        outf.write("--# of GC:           \t "+  str(self.total_num_gc) + "\n")
-        outf.write("Total Mutator time:  \t " + str(self.total_mut_time) + "\n")
-        outf.write("Total alloc size:    \t " + str(self.total_alloc_size) + "\n")
-        outf.write("Allocation rate:     \t " + str(self.allocation_rate) + "\n")
         outf.write("MPKI (L2):           \t " + str(self.mpki) + "\n")
         outf.write("CPI:                 \t " + str(self.cpi) + "\n")
         outf.write("L2 miss rate (ret):  \t " + str(self.l2missrate) + "\n")
         outf.write("L2 miss rate (pf):   \t " + str(self.l2missrate_pf) + "\n")
+        if (self.bandwidth != 0):
+          outf.write("Bandwidth (MB/sec):    \t" + str(self.bandwidth) + "\n")
+
+    def print_gc(self, outf, numlog):
+        for i in range(int(numlog, 10)):
+          print "\n[GC summary " + str(i) + "]"
+          print "Total young GC time : ", self.total_ygc_time[i]
+          print "Total full GC time:   ", self.total_fgc_time[i]
+          print "Total GC time:        ", self.total_ygc_time[i] + self.total_fgc_time[i]
+          print "--# of Young GC:      ", self.total_num_ygc[i]
+          print "--# of Full GC:       ", self.total_num_fgc[i]
+          print "--# of GC:            ", self.total_num_ygc[i] + self.total_num_fgc[i]
+          if (self.total_num_ygc[i] + self.total_num_fgc[i]) != 0:
+            self.allocation_rate.append(float(self.total_alloc_size[i]) / \
+            (float(self.total_exec_time) / float(self.total_num_ygc[i] + self.total_num_fgc[i])))
+          # print "Total Mutator time:   ", self.total_mut_time
+          print "Total alloc size:     ", self.total_alloc_size[i]
+          print "Allocation rate:      ", self.allocation_rate[i]
+          
+          outf.write("\n[GC summary " + str(i) + "]\n")
+          outf.write("Total young GC time: \t " + str(self.total_ygc_time[i]) + "\n")
+          outf.write("Total full GC time:  \t " + str(self.total_fgc_time[i]) + "\n")
+          outf.write("Total GC time:       \t " + str(self.total_ygc_time[i] + self.total_fgc_time[i]) + "\n")
+          outf.write("--# of Young GC:     \t " + str(self.total_num_ygc[i]) + "\n")
+          outf.write("--# of Full GC:      \t " + str(self.total_num_fgc[i]) + "\n")
+          outf.write("--# of GC:           \t " + str(self.total_num_ygc[i] + self.total_num_fgc[i]) + "\n")
+          outf.write("Total alloc size:    \t " + str(self.total_alloc_size[i]) + "\n")
+          outf.write("Allocation rate:     \t " + str(self.allocation_rate[i]) + "\n")
+          # outf.write("Total Mutator time:  \t " + str(self.total_mut_time) + "\n")
 
 # main -----------------
 
 # argument set / parsing
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("infile", help = "type in input file name")
+arg_parser.add_argument("ninstance", help = "number of executor instances")
 arg_parser.add_argument("-d", "--dump", help = "cat & dump file", action="store_true")
 arg_parser.add_argument("-m", "--match", help = "make name match file", action="store_true")
 arg_parser.add_argument("-g", "--gcdump", help = "gcdump for graph", action="store_true")
+arg_parser.add_argument("-v", "--vtune", help = "allow vtune analysis", action="store_true")
 args = arg_parser.parse_args()
 
 infilename_jvmlog = args.infile + ".jvmlog"
 infile_jvmlog = open(infilename_jvmlog, 'r')
-
-infilename_gclog = args.infile + ".gclog"
-infile_gclog = open(infilename_gclog, 'r')
-outfile_gclog = open(infilename_gclog + ".csv", 'w')
 
 outfilename_summary = ""
 outfilename_summary = args.infile+ ".summary"
@@ -205,10 +237,25 @@ if args.dump:
     infilename_dump = args.infile + ".alloc" + ".dump"
     infile_dump= open(infilename_dump, 'r')
 
+# parse log
 summary = gcStat()
-summary.parse_gclog(infile_gclog, outfile_gclog)
-summary.parse_jvmlog(infile_jvmlog)
-summary.print_on(outfile_summary)
+
+if not args.vtune:
+  summary.parse_jvmlog(infile_jvmlog)
+  summary.print_on(outfile_summary)
+
+if args.vtune:
+  print "v on"
+  summary.parse_vtune_stat(infile_jvmlog, outfile_summary)
+
+if args.gcdump:
+  infilename_gclog=[]
+  for i in range(int(args.ninstance, 10)):
+    infilename_gclog.append(args.infile + "."+ str(i) + ".gclog")
+    infile_gclog = open(infilename_gclog[i], 'r')
+    outfile_gclog = open(infilename_gclog[i] + ".csv", 'w')
+    summary.parse_gclog(infile_gclog, outfile_gclog, i)
+  summary.print_gc(outfile_summary, args.ninstance)
 
 if args.dump:
     hotratio = 0.1
