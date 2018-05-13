70ca8e42704398237a43fd240ec2e738cb194d8d
jangjae <rhythm2jay@gmail.com>
2018-05-13 17:35:02 +0900

    add cscope func.

--- set_env.sh
+++ set_env.sh
@@ -5,5 +5,12 @@ tmux source-file ~/.tmux.conf
 cp ./svn-config/config  $HOME/.subversion
 cp ./vim/.vimrc  $HOME
 cp -rf ./vim/.vim $HOME
+
+if [ ! -d ~/.vim/plugin ] 
+then
+    mkdir ~/.vim/plugin
+fi
+cp ./vim/cscope_maps.vim ~/.vim/plugin
+
 # vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
